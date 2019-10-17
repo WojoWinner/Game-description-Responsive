6eefae3b6af4e9b540dc72b36b4449db925dd36d
WojoWinner <mateuszw1087@gmail.com>
2019-10-18 00:04:52 +0200

    Update0.1

--- Bootstrap.js
+++ Bootstrap.js
@@ -1,7 +1,7 @@
 function search()
 {
 	var c=document.getElementById("form").value;
-	var x='<a href="'+c+'.html"/>'+c+'</a>';
+	var x='<a href="'+c+'.html"/>'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'+c+'</div>'+'</a>';
 	document.write(x);
 }
 
