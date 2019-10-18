d337ebfff738b4873ad76312d37055beb106aadc
WojoWinner <mateuszw1087@gmail.com>
2019-10-18 20:23:22 +0200

    Update0.4

--- Bootstrap.js
+++ Bootstrap.js
@@ -1,9 +1,18 @@
 function search()
 {
-	var c=document.getElementById("form").value;
-	var x='<a href="'+c+'.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
+	let c=document.getElementById("form").value;
+	let x='<a href="'+c+'.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
 	+'<img src="img/'+c+'.png" width="25px" height="25px">'+c+'</div>'+'</body>'+'</a>';
-	document.write(x);
 	
+	if(c=="Panda" || "Funny" || "Strong" || "Pizza" || "panda" || "strong" || "funny" || "pizza")
+	{
+		let z='<a href="Bootstrap.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
+		+'<img src="img/'+c+'.png" width="25px" height="25px">'+c+'</div>'+'</body>'+'</a>';
+		document.write(z);
+	}
+	else
+	{
+		document.write(x);
+	}
 }
 
