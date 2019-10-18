49027d899278d3cb2c9ccc1a938ab548b584c40d
WojoWinner <mateuszw1087@gmail.com>
2019-10-18 21:01:44 +0200

    Update0.4-Naprawienie Bledu z wyszukiwaniem

--- Bootstrap.js
+++ Bootstrap.js
@@ -4,7 +4,7 @@ function search()
 	let x='<a href="'+c+'.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
 	+'<img src="img/'+c+'.png" width="25px" height="25px">'+c+'</div>'+'</body>'+'</a>';
 	
-	if(c=="Panda" || "Funny" || "Strong" || "Pizza" || "panda" || "strong" || "funny" || "pizza")
+	if(c=="Panda" || c=="Funny" || c=="Strong" || c=="Pizza" || c=="panda" || c=="strong" || c=="funny" || c=="pizza")
 	{
 		let z='<a href="Bootstrap.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
 		+'<img src="img/'+c+'.png" width="25px" height="25px">'+c+'</div>'+'</body>'+'</a>';
