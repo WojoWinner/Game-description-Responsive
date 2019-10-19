de60d33919e4c950077841e3d240a166616eb908
WojoWinner <mateuszw1087@gmail.com>
2019-10-19 20:14:23 +0200

    Update0.6

--- Bootstrap.js
+++ Bootstrap.js
@@ -1,14 +1,22 @@
 function search()
 {
 	let c=document.getElementById("form").value;
-	let x='<a href="'+c+'.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
-	+'<img src="img/'+c+'.png" width="25px" height="25px">'+c+'</div>'+'</body>'+'</a>';
 	
-	if(c=="Panda" || c=="Funny" || c=="Strong" || c=="Pizza" || c=="panda" || c=="strong" || c=="funny" || c=="pizza")
+	
+	if(c=="Panda" || c=="Funny" || c=="Strong" || c=="Pizza" || c=="panda" || c=="strong" || c=="funny" || c=="pizza" || c=="Start" || c=="start")
 	{
 		let z='<a href="Bootstrap.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
 		+'<img src="img/'+c+'.png" width="25px" height="25px">'+c+'</div>'+'</body>'+'</a>';
 		document.write(z);
+	}
+	else if(c=="Mochi" || c=="mochi" || c=="TwoKatanas" || c=="twokatanas" || c=="Shuriken" || c=="shuriken" || c=="KunaiKnife" || c=="kunaiknife" ||
+	c=="Legier's Katana" || c=="legier's katana" || c=="Pictures" || c=="pictures" )
+	{
+		
+		let x='<a href="'+c+'.html">'+'<body style="background-color:black;">'+'<div style="background-color:green;color:white;font-size:25px;text-align:center;">'
+	+'<img src="img/'+c+'.png" width="25px" height="25px">'+c+'</div>'+'</body>'+'</a>';
+	
+	document.write(x);
 	}
 	else if(c=="")
 	{
@@ -18,7 +26,9 @@ function search()
 	}
 	else
 	{
-		document.write(x);
+		let b="Błędna nazwa przedmiotu";
+		document.getElementById("form").style.color="red";
+		document.getElementById("form").value=b;
 	}
 }
 
